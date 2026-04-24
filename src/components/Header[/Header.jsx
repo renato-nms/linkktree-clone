65c4de6a15b675f782e134328105dfa79faa7df1
renato-nms <renatonascimentomarques2@gmail.com>
2026-04-24 10:26:12 -0300

    linktree completado

--- src/components/Header[/Header.jsx
+++ src/components/Header[/Header.jsx
@@ -3,10 +3,9 @@ import "./Header.css";
 function Header() {
     return (
         <div className="perfil_conteudo">
-            <div className="perfil_imagem">
-                <h3>Renatech</h3>
+            <div className="perfil_imagem"/>
+             <h3>Renatech</h3>
                 <p>Bem vindo!</p>
-            </div>
         </div>
     )
 }
--- src/components/RedesSociais/RedesSociais.jsx
+++ src/components/RedesSociais/RedesSociais.jsx
@@ -2,9 +2,9 @@ import './RedesSociais.css'
 function RedesSociais() {
 
     const ListaRedes = [
-            { id: 1, urlNome: 'Instagram', url: 'https://www.instagram.com/renatonms11/'},
+        {id: 1, urlNome: 'Github', url:'https://github.com/renato-nms'},
             { id: 2, urlNome: 'Linkedin', url: 'https://www.linkedin.com/in/renato-marques-36a524319/'},
-            { id: 3, urlNome: 'Projeto', url: 'https://superbrasiltelessaude.com.br/'},
+            { id: 3, urlNome: 'Super Brasil Telessaude', url: 'https://superbrasiltelessaude.com.br/'},
         ]
 
         const LinksRedes = ListaRedes.map((props) =>
